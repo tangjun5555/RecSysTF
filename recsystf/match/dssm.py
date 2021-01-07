975fc90512021b878a1bc9c69ce6db9bf9d1a176
tangj <1844250138@qq.com>
2021-01-07 16:44:59 +0800

    Finish DSSM

--- recsystf/match/dssm.py
+++ recsystf/match/dssm.py
@@ -5,6 +5,8 @@
 
 import time
 import tensorflow as tf
+from recsystf.utils.normalization_ops_util import l2_norm
+from tensorflow.python.estimator.canned import optimizers
 
 
 class DSSMEstimator(tf.estimator.Estimator):
@@ -13,20 +15,149 @@ class DSSMEstimator(tf.estimator.Estimator):
                  config=None,
                  warm_start_from=None,
 
-                 weight_column=None,
+                 predict_type="logits",
+
+                 user_id_column_name="user_id",
                  user_feature_columns=None,
+                 item_id_column_name="item_id",
                  item_feature_columns=None,
 
                  user_transform_fn=None,
                  item_transform_fn=None,
-                 loss_type='pointwise',
+
+                 need_l2_norm=False,
+
+                 optimizer_name="Adam",
+                 learning_rate=0.01,
                  ):
-        assert loss_type in ['pointwise', 'pairwise']
-        
+        """
+
+        :param model_dir:
+        :param config:
+        :param warm_start_from:
+        :param user_feature_columns:
+        :param item_feature_columns:
+        :param user_transform_fn: user transform function. Follows the signature:
+            * Args:
+                * `user_net`:
+                * `features`:
+        :param item_transform_fn: item transform function. Follows the signature:
+            * Args:
+                * `net`:
+                * `features`:
+        :param optimizer_name:
+        :param learning_rate:
+        """
+
+        assert predict_type in ["logits", "user", "item"]
+
         def custom_model_fn(features, labels, mode, params=None, config=None):
-            user_vector = user_transform_fn(user_feature_columns)
-            item_vector = item_transform_fn(item_feature_columns)
-        
+            if mode == tf.estimator.ModeKeys.PREDICT and predict_type == "user":
+                user_id = features[user_id_column_name]
+                user_net = tf.feature_column.input_layer(features, feature_columns=user_feature_columns)
+                user_vector = user_transform_fn(net=user_net, features=features)
+                if need_l2_norm:
+                    user_vector = l2_norm(user_vector)
+                return tf.estimator.EstimatorSpec(
+                    mode=mode,
+                    predictions={
+                        "user_id": user_id,
+                        "user_vector": tf.reduce_join(tf.as_string(user_vector), axis=-1, separator=','),
+                    },
+                )
+
+            if mode == tf.estimator.ModeKeys.PREDICT and predict_type == "item":
+                item_id = features[item_id_column_name]
+                item_net = tf.feature_column.input_layer(features, feature_columns=item_feature_columns)
+                item_vector = item_transform_fn(net=item_net, features=features)
+                if need_l2_norm:
+                    item_vector = l2_norm(item_vector)
+                return tf.estimator.EstimatorSpec(
+                    mode=mode,
+                    predictions={
+                        "item_id": item_id,
+                        "item_vector": tf.reduce_join(tf.as_string(item_vector), axis=-1, separator=','),
+                    },
+                )
+
+            user_id = features[user_id_column_name]
+            user_net = tf.feature_column.input_layer(features, feature_columns=user_feature_columns)
+            user_vector = user_transform_fn(net=user_net, features=features)
+
+            item_id = features[item_id_column_name]
+            item_net = tf.feature_column.input_layer(features, feature_columns=item_feature_columns)
+            item_vector = item_transform_fn(net=item_net, features=features)
+
+            assert user_vector.get_shape().as_list() == item_vector.get_shape().as_list()
+
+            if need_l2_norm:
+                user_vector = l2_norm(user_vector)
+                item_vector = l2_norm(item_vector)
+
+            user_item_sim = tf.reduce_sum(
+                tf.multiply(user_vector, item_vector),
+                axis=1,
+                keep_dims=True,
+            )
+            tf.logging.info(
+                "%s DSSMEstimator custom_model_fn, user_item_sim.shape:%s" %
+                (
+                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                    user_item_sim.shape
+                )
+            )
+            sim_w = tf.get_variable("sim_w", dtype=tf.float32, shape=(1, 1), initializer=tf.ones_initializer())
+            sim_b = tf.get_variable("sim_b", dtype=tf.float32, shape=(1,), initializer=tf.zeros_initializer())
+            logits = tf.matmul(user_item_sim, tf.abs(sim_w)) + sim_b
+            logits = tf.reshape(logits, (-1,))
+            tf.logging.info(
+                "%s DSSMEstimator custom_model_fn, logits.shape:%s" %
+                (
+                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                    logits.shape
+                )
+            )
+
+            predictions = tf.nn.sigmoid(logits)
+            if mode == tf.estimator.ModeKeys.PREDICT and predict_type == "logits":
+                return tf.estimator.EstimatorSpec(
+                    mode=mode,
+                    predictions={
+                        "user_id": user_id,
+                        "user_vector": tf.reduce_join(tf.as_string(user_vector), axis=-1, separator=','),
+                        "item_id": item_id,
+                        "item_vector": tf.reduce_join(tf.as_string(item_vector), axis=-1, separator=','),
+                        "user_item_sim": user_item_sim,
+                        "predictions": predictions
+                    },
+                )
+
+            loss = tf.losses.log_loss(
+                labels=tf.cast(labels, tf.float32),
+                predictions=predictions,
+            )
+            eval_metric_ops = dict()
+            eval_metric_ops["auc"] = tf.metrics.auc(labels, predictions)
+            if mode == tf.estimator.ModeKeys.EVAL:
+                return tf.estimator.EstimatorSpec(
+                    mode=mode,
+                    loss=loss,
+                    eval_metric_ops=eval_metric_ops,
+                )
+
+            assert mode == tf.estimator.ModeKeys.TRAIN
+            optimizer_instance = optimizers.get_optimizer_instance(optimizer_name, learning_rate=learning_rate, )
+            train_op = optimizer_instance.minimize(
+                loss,
+                global_step=tf.train.get_global_step(),
+            )
+            return tf.estimator.EstimatorSpec(
+                mode=mode,
+                loss=loss,
+                train_op=train_op,
+                eval_metric_ops=eval_metric_ops,
+            )
+
         super().__init__(
             model_fn=custom_model_fn,
             model_dir=model_dir,
@@ -35,5 +166,5 @@ class DSSMEstimator(tf.estimator.Estimator):
             warm_start_from=warm_start_from,
         )
         tf.logging.info(
-            '[%s] DeepAndCrossNetworkEstimator init' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+            "[%s] DSSMEstimator init" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         )
---
+++ recsystf/utils/normalization_ops_util.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# author: tangj 1844250138@qq.com
+# time: 2021/1/7 3:48 下午
+# desc:
+
+import tensorflow as tf
+
+
+def l2_norm(input_net):
+    """
+    L2标准化
+    :param input_net:
+    :return:
+    """
+    norm = tf.norm(input_net, axis=1, keepdims=True)
+    return tf.div(input_net, tf.maximum(norm, 1e-12))
